130bebbeb643e9271ed78c84395fad3dc7845a55
st095227 <antonov.semen2015@gmail.com>
2022-12-11 20:41:08 +0300

    homework 11

---
+++ 2022.12.11-homework-11_Bash/1/Source.cpp
@@ -0,0 +1,26 @@
+#include<fstream>
+#include<iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	ifstream file("in1.txt");
+	int* arr = new int[1000];
+
+	for (int i = 0; i < 1000;++i)
+	{
+		file >> arr[i];
+	}
+	sort(arr, arr + 1000);
+	ofstream out("res1.txt");
+	for (int i = 0; i < 1000;++i)
+	{
+		out << arr[i] << ' ';
+	}
+	delete[] arr;
+	file.close(); 
+	out.close();
+	return 0;
+}
